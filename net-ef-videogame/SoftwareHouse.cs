539cd4ab03d9870e7fff48e97c961a487ce75d43
Gaspare <Gas9220@users.noreply.github.com>
2023-09-22 14:36:57 +0200

    Videogames and Software House migration

---
+++ net-ef-videogame/SoftwareHouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    [Table("softwarehouse")]
+    internal class SoftwareHouse
+    {
+        public int SoftwareHouseId { get; set; }
+        public string Name { get; set; }
+        [Column("vat_number")]
+        public int VatNumber { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public List<Videogame> Videogames { get; set; }
+
+    }
+}
---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    [Table("videogame")]
+    internal class Videogame
+    {
+        public int VideogameId { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        [Column("release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public int SoftwareHouseId { get; set; }
+        public SoftwareHouse SoftwareHouse { get; set; }
+    }
+}
